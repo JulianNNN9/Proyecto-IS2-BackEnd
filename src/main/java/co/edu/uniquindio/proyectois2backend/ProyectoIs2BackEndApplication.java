cfb20dcd9abc8fae762005a6f8586e5bd071062d
JulianNNN9 <111833091+JulianNNN9@users.noreply.github.com>
2024-09-28 10:44:06 -0500

    Segundo commit

---
+++ src/main/java/co/edu/uniquindio/proyectois2backend/ProyectoIs2BackEndApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.proyectois2backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoIs2BackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoIs2BackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/uniquindio/proyectois2backend/ProyectoIs2BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.proyectois2backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoIs2BackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
