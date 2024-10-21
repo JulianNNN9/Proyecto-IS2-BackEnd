4d9d667cabf8e06f5cbb02049eebdcef9481268c
JoanGomez <joangomezusma4@gmail.com>
2024-10-21 13:02:03 -0500

    Coreccion de controladores cambiando el service por el service Implementado

--- src/main/java/co/edu/uniquindio/proyectois2backend/controllers/ServicioController.java
+++ src/main/java/co/edu/uniquindio/proyectois2backend/controllers/ServicioController.java
@@ -3,6 +3,7 @@ package co.edu.uniquindio.proyectois2backend.controllers;
 import co.edu.uniquindio.proyectois2backend.dto.cita.InformacionServicioDTO;
 import co.edu.uniquindio.proyectois2backend.model.Cliente;
 import co.edu.uniquindio.proyectois2backend.services.implementacion.ClienteServiceImple;
+import co.edu.uniquindio.proyectois2backend.services.implementacion.ServicioServiceImple;
 import co.edu.uniquindio.proyectois2backend.services.interfaces.ServicioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,12 +18,12 @@ import java.util.List;
 @RequestMapping("/api/servicios")
 public class ServicioController {
 
-    private final ServicioService servicioService;
+    private final ServicioServiceImple servicioServiceimple;
 
     // Endpoint para obtener todos los servicios
     @GetMapping("/obtener-servicios")
     public ResponseEntity<List<InformacionServicioDTO>> obtenerTodosLosServicios() {
-        List<InformacionServicioDTO> servicios = servicioService.obtenerTodosLosServicios();
+        List<InformacionServicioDTO> servicios = servicioServiceimple.obtenerTodosLosServicios();
         return ResponseEntity.ok(servicios);
     }
 }
