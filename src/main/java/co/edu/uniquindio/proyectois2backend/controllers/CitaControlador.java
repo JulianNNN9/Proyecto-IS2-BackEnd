b85eeba8785da8052fba5f077e220c2df403cfb7
Johan Noe Londoño Salazar <111825049+JohanLondono@users.noreply.github.com>
2024-10-20 19:51:29 -0500

    Se agregó los endpoints para cancelar y reprogramar cita (se corrijio tambien estos metodos) y se agregó los endpoints y funcionalidades para modificar citas pendientes, finalizar cita y agregar comentarios y propinas a las citas

--- src/main/java/co/edu/uniquindio/proyectois2backend/controllers/CitaControlador.java
+++ src/main/java/co/edu/uniquindio/proyectois2backend/controllers/CitaControlador.java
@@ -3,12 +3,17 @@ package co.edu.uniquindio.proyectois2backend.controllers;
 import co.edu.uniquindio.proyectois2backend.dto.MensajeDTO;
 import co.edu.uniquindio.proyectois2backend.dto.cita.CrearCitaDTO;
 import co.edu.uniquindio.proyectois2backend.dto.cita.InformacionCitasClienteDTO;
+import co.edu.uniquindio.proyectois2backend.dto.cita.ModificarCitaDTO;
+import co.edu.uniquindio.proyectois2backend.model.Cita;
 import co.edu.uniquindio.proyectois2backend.services.interfaces.CitaService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @CrossOrigin(origins = "*")
@@ -30,10 +35,81 @@ public class CitaControlador {
         citaService.confirmarCita(citaId);
         return ResponseEntity.ok().body(new MensajeDTO<>(false, "Cita confirmada correctamente"));
     }
-    @GetMapping("/{clienteId}/historial")
+    @GetMapping("/{clienteId}/historial-citas")
     public ResponseEntity<List<InformacionCitasClienteDTO>> obtenerHistorialCliente(@PathVariable Long clienteId) {
         List<InformacionCitasClienteDTO> historial = citaService.obtenerHistorialCliente(clienteId);
         return ResponseEntity.ok(historial);
+    }
+    // Endpoint para cancelar una cita
+    @PutMapping("/{citaId}/cancelar-cita")
+    public ResponseEntity<MensajeDTO<String>> cancelarCita(@PathVariable Long citaId) {
+        try {
+            Cita citaCancelada = citaService.cancelarCita(citaId);
+            return ResponseEntity.ok(new MensajeDTO<>(false, "La cita ha sido cancelada exitosamente"));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new MensajeDTO<>(true, e.getMessage()));
+        }
+    }
+    // Endpoint para cancelar una cita
+    @PutMapping("/{citaId}/finalizar-cita")
+    public ResponseEntity<MensajeDTO<String>> finalizarCita(@PathVariable Long citaId) {
+        try {
+            citaService.finalizarCita(citaId);
+            return ResponseEntity.ok(new MensajeDTO<>(false, "La cita ha sido finalizada"));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new MensajeDTO<>(true, e.getMessage()));
+        }
+    }
+    @PutMapping("/{citaId}/modificar-cita-pendiente")
+    public ResponseEntity<MensajeDTO<String>> modificarCita(@PathVariable Long citaId, @RequestBody ModificarCitaDTO modificarCitaDTO) {
+        System.out.println(citaId);
+        System.out.println(modificarCitaDTO);
+        try {
+            Cita citaModificada = citaService.modificarCitaPendiente(citaId, modificarCitaDTO);
+            return ResponseEntity.ok(new MensajeDTO<>(false, "Cita modificada exitosamente"));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new MensajeDTO<>(true, e.getMessage()));
+        }
+    }
+    // Endpoint para modificar el comentario de una cita
+    @PutMapping("/{citaId}/comentario-cita")
+    public ResponseEntity<MensajeDTO<String>> modificarComentarioCita(
+            @PathVariable Long citaId,
+            @RequestParam String comentario) {
+        System.out.println(citaId);
+        System.out.println(comentario);
+        try {
+            Cita citaModificada = citaService.modificarComentarioCita(citaId, comentario);
+            return ResponseEntity.ok(new MensajeDTO<>(false, "Su comentario se agregó correctamente"));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new MensajeDTO<>(true, e.getMessage()));
+        }
+    }
+
+    // Endpoint para agregar propina a una cita
+    @PutMapping("/{citaId}/propina-cita")
+    public ResponseEntity<MensajeDTO<String>> agregarPropinaCita(
+            @PathVariable Long citaId,
+            @RequestParam double propina) {
+        try {
+            Cita citaModificada = citaService.agregarPropinaCita(citaId, propina);
+            return ResponseEntity.ok(new MensajeDTO<>(false, "Propina Agregada"));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new MensajeDTO<>(true, e.getMessage()));
+        }
+    }
+
+    // Endpoint para reprogramar una cita
+    @PutMapping("/{citaId}/reprogramar-cita")
+    public ResponseEntity<MensajeDTO<String>> reprogramarCita(
+            @PathVariable Long citaId,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime nuevaFecha) {
+        try {
+            Cita citaReprogramada = citaService.reprogramarCita(citaId, nuevaFecha);
+            return ResponseEntity.ok(new MensajeDTO<>(false, "Su cita se reprogramó exitosamente"));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new MensajeDTO<>(true, e.getMessage()));
+        }
     }
     @PostMapping("/saludo")
     public ResponseEntity<String> saludo() {
--- src/main/java/co/edu/uniquindio/proyectois2backend/services/interfaces/CitaService.java
+++ src/main/java/co/edu/uniquindio/proyectois2backend/services/interfaces/CitaService.java
@@ -3,6 +3,7 @@ package co.edu.uniquindio.proyectois2backend.services.interfaces;
 import co.edu.uniquindio.proyectois2backend.dto.cita.ConfirmarCitaDTO;
 import co.edu.uniquindio.proyectois2backend.dto.cita.CrearCitaDTO;
 import co.edu.uniquindio.proyectois2backend.dto.cita.InformacionCitasClienteDTO;
+import co.edu.uniquindio.proyectois2backend.dto.cita.ModificarCitaDTO;
 import co.edu.uniquindio.proyectois2backend.model.Cita;
 
 import java.time.LocalDateTime;
@@ -15,5 +16,9 @@ public interface CitaService {
     void confirmarCita(Long id) throws Exception;
     List<InformacionCitasClienteDTO> obtenerHistorialCliente(Long clienteId);
     Cita cancelarCita(Long citaId) throws Exception;
+    Cita modificarCitaPendiente(Long citaId, ModificarCitaDTO modificarCitaDTO) throws Exception;
+    Cita modificarComentarioCita(Long citaId, String comentario) throws Exception;
+    Cita agregarPropinaCita(Long citaId, double propina) throws Exception;
     Cita reprogramarCita(Long citaId, LocalDateTime nuevaFecha) throws Exception;
+    void finalizarCita(Long citaId) throws Exception;
 }
