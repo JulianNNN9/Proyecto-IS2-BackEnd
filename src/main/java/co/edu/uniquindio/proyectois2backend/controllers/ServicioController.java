3bbe9ec500d75fe073c2e6663a54280d4ed2ef5b
JoanGomez <joangomezusma4@gmail.com>
2024-10-21 19:55:07 -0500

    creacion de controladores nuevos, Estilistas, Productos; DTOs nuevos, Estilistas, Productos; Modificacion de algunas clases

--- src/main/java/co/edu/uniquindio/proyectois2backend/controllers/ServicioController.java
+++ src/main/java/co/edu/uniquindio/proyectois2backend/controllers/ServicioController.java
@@ -1,12 +1,8 @@
 package co.edu.uniquindio.proyectois2backend.controllers;
 
 import co.edu.uniquindio.proyectois2backend.dto.cita.InformacionServicioDTO;
-import co.edu.uniquindio.proyectois2backend.model.Cliente;
-import co.edu.uniquindio.proyectois2backend.services.implementacion.ClienteServiceImple;
-import co.edu.uniquindio.proyectois2backend.services.implementacion.ServicioServiceImple;
 import co.edu.uniquindio.proyectois2backend.services.interfaces.ServicioService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,12 +14,12 @@ import java.util.List;
 @RequestMapping("/api/servicios")
 public class ServicioController {
 
-    private final ServicioServiceImple servicioServiceimple;
+    private final ServicioService servicioService;
 
     // Endpoint para obtener todos los servicios
     @GetMapping("/obtener-servicios")
-    public ResponseEntity<List<InformacionServicioDTO>> obtenerTodosLosServicios() {
-        List<InformacionServicioDTO> servicios = servicioServiceimple.obtenerTodosLosServicios();
+    public ResponseEntity<List<InformacionServicioDTO>> obtenerTodosLosServicios() throws Exception {
+        List<InformacionServicioDTO> servicios = servicioService.obtenerTodosLosServicios();
         return ResponseEntity.ok(servicios);
     }
 }
--- src/main/java/co/edu/uniquindio/proyectois2backend/services/implementacion/EstilistaServiceImple.java
+++ src/main/java/co/edu/uniquindio/proyectois2backend/services/implementacion/EstilistaServiceImple.java
@@ -1,8 +1,35 @@
 package co.edu.uniquindio.proyectois2backend.services.implementacion;
 
+import co.edu.uniquindio.proyectois2backend.dto.cita.InformacionEstilistaDTO;
+import co.edu.uniquindio.proyectois2backend.model.Estilista;
+import co.edu.uniquindio.proyectois2backend.repositories.EstilistaRepository;
 import co.edu.uniquindio.proyectois2backend.services.interfaces.EstilistaService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
 @Service
+@Transactional
+@RequiredArgsConstructor
 public class EstilistaServiceImple implements EstilistaService {
+
+    private final EstilistaRepository estilistaRepository;
+
+    @Override
+    public List<InformacionEstilistaDTO> obtenerTodosLosEstilistas() {
+        List<Estilista> estilistas = estilistaRepository.findAll();
+
+        // Convertir los servicios a DTO
+        return estilistas.stream()
+                .map(estilista -> new InformacionEstilistaDTO(
+                        estilista.getId(),
+                        estilista.getNombre(),
+                        estilista.getCorreo()
+                ))
+                .collect(Collectors.toList());
+    }
 }
